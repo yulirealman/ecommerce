f17cf73f64535b5ec39d3176a785c52c7cb1bcca
yulirealman <yulirealman@gmail.com>
2026-01-18 20:57:08 +0800

    feat: implement routing with React Router and add CheckoutPage component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 
 import './App.css'
+import {Routes,Route} from 'react-router'
 import HomePage from './pages/HomePage'
 function App() {
 
 
   return (
     <>
-      <HomePage />
+      <Routes> 
+        <Route path="/" element={<HomePage />} />
+        <Route path="/checkout" element={<div>
+          this is checkout
+        </div>} />
+      </Routes>
+
     </>
   )
 }
