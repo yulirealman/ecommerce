5640f65f3771dc071e4660bf6d5933762533e070
yulirealman <yulirealman@gmail.com>
2026-01-19 09:08:09 +0800

    Order page implement

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import { Routes, Route } from 'react-router'
 import {HomePage} from './pages/HomePage'
 import {CheckOutPage} from './pages/CheckOutPage'
+import {OrdersPage} from './pages/OrdersPage'
 function App() {
 
 
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/checkout" element={<CheckOutPage />} />
+        <Route path="/orders" element={<OrdersPage />} />
       </Routes>
 
     </>
