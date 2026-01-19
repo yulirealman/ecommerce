9e28c1df62a94f5ce6b514c3add0ae6382ffe47a
yulirealman <yulirealman@gmail.com>
2026-01-19 10:27:04 +0800

    dynamic display

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import './HomePage.css'
 
-import {Header} from '../components/Header'
+import { Header } from '../components/Header'
+import { products } from '../starting-code/data/products'
 export function HomePage() {
     return (
         <>
@@ -9,54 +10,58 @@ export function HomePage() {
 
             <div className="home-page">
                 <div className="products-grid">
-                    <div className="product-container">
-                        <div className="product-image-container">
-                            <img className="product-image"
-                                src="images/products/athletic-cotton-socks-6-pairs.jpg" />
-                        </div>
-
-                        <div className="product-name limit-text-to-2-lines">
-                            Black and Gray Athletic Cotton Socks - 6 Pairs
-                        </div>
-
-                        <div className="product-rating-container">
-                            <img className="product-rating-stars"
-                                src="images/ratings/rating-45.png" />
-                            <div className="product-rating-count link-primary">
-                                87
+                    {products.map((product) => (
+                        <div key={product.id} className="product-container">
+                            <div className="product-image-container">
+                                <img className="product-image"
+                                    src={product.image} />
                             </div>
+
+                            <div className="product-name limit-text-to-2-lines">
+                                {product.name}
+                            </div>
+
+                            <div className="product-rating-container">
+                                <img className="product-rating-stars"
+                                    src={`images/ratings/rating-${product.rating.stars*10}.png`} />
+                                <div className="product-rating-count link-primary">
+                                    {product.rating.count}
+                                </div>
+                            </div>
+
+                            <div className="product-price">
+                                ${(product.priceCents / 100).toFixed(2)}
+                            </div>
+
+                            <div className="product-quantity-container">
+                                <select>
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                    <option value="6">6</option>
+                                    <option value="7">7</option>
+                                    <option value="8">8</option>
+                                    <option value="9">9</option>
+                                    <option value="10">10</option>
+                                </select>
+                            </div>
+
+                            <div className="product-spacer"></div>
+
+                            <div className="added-to-cart">
+                                <img src="images/icons/checkmark.png" />
+                                Added
+                            </div>
+
+                            <button className="add-to-cart-button button-primary">
+                                Add to Cart
+                            </button>
                         </div>
+                    ))}
 
-                        <div className="product-price">
-                            $10.90
-                        </div>
 
-                        <div className="product-quantity-container">
-                            <select>
-                                <option value="1">1</option>
-                                <option value="2">2</option>
-                                <option value="3">3</option>
-                                <option value="4">4</option>
-                                <option value="5">5</option>
-                                <option value="6">6</option>
-                                <option value="7">7</option>
-                                <option value="8">8</option>
-                                <option value="9">9</option>
-                                <option value="10">10</option>
-                            </select>
-                        </div>
-
-                        <div className="product-spacer"></div>
-
-                        <div className="added-to-cart">
-                            <img src="images/icons/checkmark.png" />
-                            Added
-                        </div>
-
-                        <button className="add-to-cart-button button-primary">
-                            Add to Cart
-                        </button>
-                    </div>
 
                     <div className="product-container">
                         <div className="product-image-container">
