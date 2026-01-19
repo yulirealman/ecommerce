532bad87524a3324d3094806f49db4246f2b5e18
yulirealman <yulirealman@gmail.com>
2026-01-19 09:11:46 +0800

    update route for tracking

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Routes, Route } from 'react-router'
 import {HomePage} from './pages/HomePage'
 import {CheckOutPage} from './pages/CheckOutPage'
 import {OrdersPage} from './pages/OrdersPage'
+import {TrackingPage} from './pages/TrackingPage'
 function App() {
 
 
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/checkout" element={<CheckOutPage />} />
         <Route path="/orders" element={<OrdersPage />} />
+        <Route path="/tracking" element={<TrackingPage />} />
       </Routes>
 
     </>
