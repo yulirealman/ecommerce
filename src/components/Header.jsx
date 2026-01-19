96cc3d41bcd613f730f6d4c4271de05fa5a1b151
yulirealman <yulirealman@gmail.com>
2026-01-19 09:30:29 +0800

    instead of reloading page after click header, use link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,15 @@
 import './header.css'
+import {Link} from 'react-router'
 export function Header() {
     return (
         <div className="header">
             <div className="left-section">
-                <a href="/" className="header-link">
+                <Link to="/" className="header-link">
                     <img className="logo"
                         src="images/logo-white.png" />
                     <img className="mobile-logo"
                         src="images/mobile-logo-white.png" />
-                </a>
+                </Link>
             </div>
 
             <div className="middle-section">
@@ -20,16 +21,16 @@ export function Header() {
             </div>
 
             <div className="right-section">
-                <a className="orders-link header-link" href="/orders">
+                <Link to="/orders" className="orders-link header-link">
 
                     <span className="orders-text">Orders</span>
-                </a>
+                </Link>
 
-                <a className="cart-link header-link" href="/checkout">
+                <Link to="/checkout" className="cart-link header-link">
                     <img className="cart-icon" src="images/icons/cart-icon.png" />
                     <div className="cart-quantity">3</div>
                     <div className="cart-text">Cart</div>
-                </a>
+                </Link>
             </div>
         </div>
     )
